57b86abfaa31068eeb43a8490797215cf5d83184
Zach <zach_oakes@outlook.com>
2023-10-29 11:45:14 -0500

    figma scratch

--- pages/index.jsx
+++ pages/index.jsx
@@ -94,6 +94,42 @@ export default function HomePage() {
       <ChartComponent data={initialData} />
       <MultiLineChartComponent />
       <HighchartsExample />
+      <div className="w-[1120px] h-[250px] relative">
+            <div className="w-[1120px] h-[250px] left-0 top-0 absolute bg-zinc-300" />
+            <div className="w-[668px] h-[200px] left-[357px] top-[25px] absolute">
+                <div className="w-[0px] h-40 left-[65px] top-[5px] absolute">
+                </div>
+                <div className="w-[442.85px] h-[158.48px] left-[143px] top-[6.99px] absolute">
+                </div>
+                <div className="w-[577.46px] h-[162.61px] left-[66.37px] top-[3.40px] absolute">
+                </div>
+                <div className="w-[559.86px] h-[156.09px] left-[74.10px] top-[12px] absolute">
+                </div>
+                <div className="w-[568.84px] h-[133.09px] left-[83.51px] top-[32.91px] absolute">
+                </div>
+            </div>
+            <div className="left-[48px] top-[199px] absolute text-black text-opacity-20 text-[17px] font-normal font-['Inter']">Rolling 20d Average</div>
+            <div className="left-[206px] top-[118px] absolute text-green-600 text-[35px] font-normal font-['Inter']">+.03</div>
+            <div className="w-[151px] h-[77px] left-[38px] top-[100px] absolute text-black text-[75px] font-bold font-['Inter']">2.14</div>
+            <div className="w-[318px] left-[33px] top-[32px] absolute text-black text-[50px] font-bold font-['Inter']">Sharpe Ratio</div>
+        </div>
+
+        <div className="w-[143px] h-[119px] relative bg-zinc-300">
+              <div className="p-2.5 left-0 top-[-0px] absolute justify-center items-center gap-2.5 inline-flex">
+                  <div className="text-black text-xs font-bold font-['Inter']">Sharpe Ratio</div>
+              </div>
+              <div className="p-2.5 left-0 top-[34px] absolute justify-start items-center gap-2.5 inline-flex">
+                  <div className="text-black text-[25px] font-bold font-['Inter']">2.14</div>
+              </div>
+              <div className="pl-px pr-[5px] py-1 left-[47.48px] top-[41.46px] absolute justify-start items-start inline-flex" />
+              <div className="p-2.5 left-0 top-[87px] absolute justify-center items-center inline-flex">
+                  <div className="text-black text-opacity-20 text-[10px] font-normal font-['Inter']">Rolling 20d</div>
+              </div>
+              <div className="w-[47px] h-[35px] left-[95px] top-0 absolute justify-center items-center inline-flex">
+                  <div className="text-black text-xs font-normal font-['Inter']">+.03</div>
+              </div>
+          </div>
+
 
       <MyDataGrid />
     </div>
