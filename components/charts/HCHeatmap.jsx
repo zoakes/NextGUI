7592b5b5435f949c3fba7e854b85fa0860ab40ec
Zach <zach_oakes@outlook.com>
2023-10-30 17:10:10 -0500

    heatmap was working, wtf?

--- components/charts/HCHeatmap.jsx
+++ components/charts/HCHeatmap.jsx
@@ -1,45 +1,55 @@
-// pages/heatmap.js or in a component file
-
+import React from 'react';
 import Highcharts from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
-import heatmap from 'highcharts/modules/heatmap';
+// import Heatmap from 'highcharts/modules/heatmap';
+// wtf? 
+// Heatmap(Highcharts);
 
-// Initialize the heatmap module
-heatmap(Highcharts);
 
-const Heatmap = () => {
-  const options = {
-    chart: {
-      type: 'heatmap',
-      plotBorderWidth: 1,
-    },
-    title: {
-      text: 'Exposure',
-    },
-    xAxis: {
-      categories: ['Category 1', 'Category 2', 'Category 3'],
-    },
-    yAxis: {
-      categories: ['Y 1', 'Y 2', 'Y 3'],
-      title: null,
-    },
-    colorAxis: {
-      min: 0,
-      minColor: '#FFFFFF',
-      maxColor: Highcharts.getOptions().colors[0],
-    },
-    series: [{
-      name: 'Sales per employee',
-      borderWidth: 1,
-      data: [[0, 0, 10], [0, 1, 19], [0, 2, 8], [1, 0, 24], [1, 1, 67], [1, 2, 10], [2, 0, 92], [2, 1, 58], [2, 2, 78], ],
-      dataLabels: {
-        enabled: true,
-        color: '#000000',
-      },
-    }],
-  };
+export default class HeatmapComponent extends React.Component {
+    generateData(rows, cols) {
+        let data = [];
+        for (let i = 0; i < rows; i++) {
+            for (let j = 0; j < cols; j++) {
+                data.push([i, j, Math.floor(Math.random() * 100)]);
+            }
+        }
+        return data;
+    }
 
-  return <HighchartsReact highcharts={Highcharts} options={options} />;
-};
+    render() {
+        const options = {
+            chart: {
+                type: 'heatmap',
+            },
+            title: {
+                text: 'Factor Exposure',
+            },
+            xAxis: {
+                categories: Array.from({ length: 20 }, (_, i) => `Factor ${i + 1}`),
+            },
+            yAxis: {
+                categories: Array.from({ length: 20 }, (_, i) => `ID ${i + 1}`),
+                title: null,
+            },
+            colorAxis: {
+                min: 0,
+                minColor: '#FFFFFF',
+                maxColor: 'blue', //Highcharts.getOptions().colors[0],
+            },
+            legend: {
+                align: 'right',
+                layout: 'vertical',
+                margin: 0,
+                verticalAlign: 'middle',
+            },
+            series: [{
+                name: 'Data',
+                borderWidth: 1,
+                data: this.generateData(20, 20),
+            }],
+        };
 
-export default Heatmap;
+        return <HighchartsReact highcharts={Highcharts} options={options} />;
+    }
+}
--- pages/exposure.jsx
+++ pages/exposure.jsx
@@ -1,15 +1,71 @@
 import SearchAppBar from '../components/SearchBar';
-import { Box } from '@mui/material';
+import { Box, Grid } from '@mui/material';
 import TreeExample from '../components/charts/TreeMap';
 import Heatmap from '../components/charts/HCHeatmap';
+import CardInvertedColors from '../components/cards/JoyCard';
+import MyDataGrid from '../components/tables/DataTable';
 
 export default function ExposurePage() {
+  const card_props = [
+    {title: "Gross Exposure", value: "$425.5M", progress: 40},
+    {title: "Net Exposure", value: "$12.15M", progress: 10},
+    {title: "Max (%)", value: "11%", progress: 10},
+    {title: "Min (%)", value: "-4%", progress: 17},
+  ]
+
+  // Columns
+  const defaultColumns = [
+    { field: 'id', headerName: 'ID', width: 90, sortable: true },
+    { field: 'child', headerName: 'Child', width: 120, sortable: true },
+    { field: 'parent', headerName: 'Parent', width: 120, sortable: true },
+    ...Array.from({ length: 20 }, (_, i) => ({ 
+        field: `f${i+1}`, 
+        headerName: `F${i+1}`, 
+        width: 25, 
+        sortable: true 
+    }))
+  ];
+
+  // Sample tickers for child and parent
+  const tickers = ['AAPL', 'MSFT', 'GOOGL', 'AMZN', 'FB', 'TSLA', 'BRK.A', 'V', 'JNJ', 'WMT'];
+
+  // Generate random decimal between 0 and 1
+  const randomDecimal = () => parseFloat((Math.random()).toFixed(2));
+
+  // Rows
+  const defaultRows = Array.from({ length: 100 }, (_, i) => {
+      let obj = {
+          id: i+1,
+          child: tickers[Math.floor(Math.random() * tickers.length)],
+          parent: tickers[Math.floor(Math.random() * tickers.length)]
+      };
+
+      // Fill in random decimals for factors
+      for (let j = 1; j <= 20; j++) {
+          obj[`f${j}`] = randomDecimal();
+      }
+      return obj;
+  });
+
   return (
     <div>
         <SearchAppBar />
         <Box>
-            <TreeExample />
-            <Heatmap />
+            <Grid container spacing={2} className='my-5'>
+              {card_props.map((item) => (
+                <Grid item xs={12} sm={6} md={4} lg={3} key={item}>
+                  <div className='flex items-center justify-center'>
+                    <CardInvertedColors 
+                      title={item.title} 
+                      progress={item.progress} 
+                      value={item.value}
+                    />
+                  </div>
+                </Grid>
+              ))}
+            </Grid>
+            {/* <Heatmap /> FUCK -- was working... */}
+            <MyDataGrid className='my-20' columns={defaultColumns} rows={defaultRows} />
         </Box>
     </div>
    
