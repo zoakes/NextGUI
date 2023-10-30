a2d82fba50051ad1e66e9808847fc2a8d3e7a288
Zach <zach_oakes@outlook.com>
2023-10-30 16:43:18 -0500

    close...

--- components/charts/HCHeatmap.jsx
+++ components/charts/HCHeatmap.jsx
@@ -4,20 +4,6 @@ import Highcharts from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
 import heatmap from 'highcharts/modules/heatmap';
 
-const generateData = (pointCount) => {
-    let data = [];
-    for (let i = 0; i < pointCount; i++) {
-      for (let j = 0; j < pointCount; j++) {
-        // Random value for demonstration
-        const value = Math.floor(Math.random() * 100);
-        data.push([i, j, value]);
-      }
-    }
-    return data;
-  };
-  
-const heatmapData = generateData(70);
-
 // Initialize the heatmap module
 heatmap(Highcharts);
 
@@ -28,7 +14,7 @@ const Heatmap = () => {
       plotBorderWidth: 1,
     },
     title: {
-      text: 'Sample Heatmap',
+      text: 'Exposure',
     },
     xAxis: {
       categories: ['Category 1', 'Category 2', 'Category 3'],
@@ -45,7 +31,7 @@ const Heatmap = () => {
     series: [{
       name: 'Sales per employee',
       borderWidth: 1,
-      data: [[0, 0, 10], [0, 1, 19], [0, 2, 8], [1, 0, 24], [1, 1, 67], [1, 2, 10], [2, 0, 92], [2, 1, 58], [2, 2, 78]],
+      data: [[0, 0, 10], [0, 1, 19], [0, 2, 8], [1, 0, 24], [1, 1, 67], [1, 2, 10], [2, 0, 92], [2, 1, 58], [2, 2, 78], ],
       dataLabels: {
         enabled: true,
         color: '#000000',
