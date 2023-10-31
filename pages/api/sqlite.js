b01953a435d64bf64cea797e70998b03f5c9b04a
Zach <zach_oakes@outlook.com>
2023-10-31 10:28:51 -0500

    sqlite added as an api route

---
+++ pages/api/sqlite.js
@@ -0,0 +1,29 @@
+import sqlite3 from 'sqlite3';
+import { open } from 'sqlite';
+
+async function openDb() {
+  return open({
+    filename: '/path/to/your/database.sqlite',
+    driver: sqlite3.Database
+  });
+}
+
+export default async function handler(req, res) {
+  const db = await openDb();
+
+  const data = await db.all('SELECT * FROM your_table');
+  res.json(data);
+}
+
+/*
+
+// this is read only, so fairly safe from sql-injection type stuff, can make it a read only key.
+
+async function loadData() {
+  const response = await fetch('/api/sqlite.js'); // can make routes dynamic, or per .js
+  const data = await response.json();
+  // Do something with the data
+}
+// likely in useEffect, in a component
+
+*/
